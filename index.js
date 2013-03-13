943a413c09de73673592a27f0775cfb5e6feb5d0
Simone Lusenti <simone@plasticpanda.com>
2013-03-13 18:23:38 +0100

    Updates

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-/*jshint node:true, laxcomma:true, eqnull:true, indent:2 */
+#!/usr/bin/env node
+/*jshint node:true, laxcomma:true, eqnull:true, indent:2, es5:true */
 
 'use strict';
 
+require('sugar');
+
 var jade = require('jade')
-  , fs = require('fs');
+  , fs = require('fs')
+  , clog = require('clog')
+  , optimist = require('optimist')
+    .usage('Create HTML i18n templates.\nUsage: $0 tests/template.jade [--translated tests/locale/it.json] [--output tests/output]')
+    .describe('translated', 'File with translated strings')
+    .describe('output', 'Output directory (must exist)')
+    .describe('h', 'Print this help')
+    .describe('init', 'Create an initial translation file')
+    .default({ translated: 'locale/' + process.env.LANG + '.json', output: '.' })
+    .alias('t', 'translated')
+    .alias('o', 'output')
+    .alias('h', 'help')
+  , argv = optimist.argv;
 
 
-var map = {
-  h1: {
-    original: 'It\'s me,',
-    translated: 'Sono io,'
-  },
-  p1: {
-    original: 'Foo!',
-    translated: 'Pippo!'
-  },
-  fanculo: {
-    original: 'fucsia!',
-    translated: 'Gina'
-  }
-};
+if (argv.h) {
+  optimist.showHelp();
+  process.exit(1);
+}
+
+if (argv.init) {
+  fs.writeFileSync(argv.translated, '{}');
+}
+
+var translated = JSON.parse(fs.readFileSync(argv.translated))
+  , source = fs.readFileSync(argv._[0]);
 
 
 jade.filters.trans = function (str, params) {
   var ctx = params.context;
   
-  if (map[ctx] != null) {
-    if (str !== map[ctx].original) {
-      map[ctx].original = str;
-      map[ctx].fuzzy = true;
+  if (translated[ctx] != null) {
+    if (str !== translated[ctx].original) {
+      translated[ctx].original = str;
+      translated[ctx].fuzzy = true;
     }
-    return map[ctx].translated;
+    return translated[ctx].translated;
   } else {
     console.log('Missing translation for ' + str);
-    map[ctx] = {
+    translated[ctx] = {
       original: str,
       translated: str,
       fuzzy: true
@@ -42,6 +54,7 @@ jade.filters.trans = function (str, params) {
   }
 };
 
-var page = jade.compile(fs.readFileSync('tests/index.jade'))();
-console.log(page);
-console.log('map', map);
+var page = jade.compile(source)();
+
+fs.writeFileSync('output.html', page);
+fs.writeFileSync(argv.translated, JSON.stringify(translated));
