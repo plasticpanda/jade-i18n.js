bfe537d826108acb1a1f31f3173407ebbc67017a
Simone Lusenti <simone@plasticpanda.com>
2013-03-13 19:36:21 +0100

    Working :)

---
+++ README.md
@@ -0,0 +1,5 @@
+# i18n.li #
+
+## Example
+    ./index.js tests/index.jade -t tests/locale/it.json --update > output.html
+
--- index.js
+++ index.js
@@ -6,17 +6,15 @@
 var clog = require('clog')
   , fs = require('fs')
   , optimist = require('optimist')
-    .usage('Create HTML i18n templates.\nUsage: $0 tests/template.jade [--translations tests/locale/it.json] [--output .]')
+    .usage('Create HTML i18n templates.\nUsage: $0 tests/template.jade [--translations tests/locale/it.json]')
     .describe('translations', 'File with translations strings')
-    .describe('output', 'Output directory (must exist)')
     .describe('init', 'Create an initial translation file')
     .describe('update', 'Update the translation file with new strings that may have been added')
     .describe('h', 'Print this help')
     .boolean('init')
     .boolean('update')
-    .default({ translations: 'locale/' + process.env.LANG + '.json', output: 'output.html' })
+    .default({ translations: 'locale/' + process.env.LANG + '.json', update: false })
     .alias('t', 'translations')
-    .alias('o', 'output')
     .alias('h', 'help')
   , argv = optimist.argv
   , i18n = require('./lib/i18n.js');
@@ -34,25 +32,22 @@ if (argv.init) {
 }
 
 
-var source = fs.readFileSync(argv._[0])
+var sourceFile = argv._[0]
+  , source = fs.readFileSync(sourceFile)
   , strings = JSON.parse(fs.readFileSync(argv.translations))
   , res = i18n.compile(source, strings)
   , compiled = res[0]
   , strings_updated = res[1];
 
 
-// Write compiled file
-if (argv.output === '-') {
-  process.stdout.write(compiled);
-} else {
-  fs.writeFileSync(argv.output, compiled);
-}
-
-
 // Update translation file
 if (argv.update) {
-  console.log('updating strings...');
-  
+  fs.writeFileSync(argv.translations, JSON.stringify(strings_updated));
+  clog.debug('Translations file updated.');
 } else {
   clog.info('If you have missing translations you can run this script with the --update flag.');
 }
+
+
+process.stdout.write(compiled + '\n');
+clog.info('OK');
